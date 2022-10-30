b7fcda15727e7c244e8b4ea5b0a873b8c073e1f3
Angel Romero <mrangelrcd@gmail.com>
2022-10-30 22:10:15 +0100

    Visual style finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,22 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    // Añadir color de las web, dentro de theme
-    colors: {
-      web: {
-        boton: "#56B6CB",
-        fondo: "#C9D5E0",
-        300: "#67e8f9",
-        400: "#22d3ee",
-        500: "#06b6d4",
-        600: "#0891b2",
-        700: "#0e7490",
-        800: "#155e75",
-        900: "#164e63",
-      },
-    },
-
     extend: {
+
+      colors: {
+        web: {
+          boton: "#56B6CB",
+          fondo: "#C9D5E0",
+          300: "#67e8f9",
+          400: "#22d3ee",
+          500: "#06b6d4",
+          600: "#0891b2",
+          700: "#0e7490",
+          800: "#155e75",
+          900: "#164e63",
+        },
+      },
+
       fontFamily: {
         bodyFont: ["Bluetea Regular", "cursive"],
       }
